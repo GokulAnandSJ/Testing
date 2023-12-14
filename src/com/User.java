050b2f07283ee95adeba67e3ecf8921f219af15a
GokulAnandSJ <gokulanand0110@gmail.com>
2023-12-14 17:25:49 +0530

    User Project

---
+++ src/com/User.java
@@ -0,0 +1,26 @@
+package com;
+
+class User {
+
+	private int userid;
+	private String username;
+	private long mobilename;
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public long getMobilename() {
+		return mobilename;
+	}
+	public void setMobilename(long mobilename) {
+		this.mobilename = mobilename;
+	}
+}
